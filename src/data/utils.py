da57a7339c2613ed9962f028a5ff8cb3aabf690f
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-10-19 15:27:34 -0400

    added pr data

--- src/data/utils.py
+++ src/data/utils.py
@@ -52,7 +52,6 @@ class DiffReg(cleanData, DataPull):
             (pl.col("total_employment") != 0) & (pl.col("total_wages") != 0)
         )
         df_qcew = df_qcew.with_columns(
-            wages_employee=pl.col("total_wages") / pl.col("total_employment"),
             sector=pl.col("naics_code").str.slice(0, 2),
         )
         return df_qcew
@@ -97,8 +96,86 @@ class DiffReg(cleanData, DataPull):
         gdf = gpd.sjoin(
             gdf_zips,
             gdf_county[["geometry", "area_fips"]],
-            how="inner",
+            how="left",
             predicate="intersects",
         ).drop("index_right", axis=1)
 
+        gdf = gdf.drop_duplicates(subset=["zipcode"])
+
         return gdf
+
+    def regular_data(self, naics: str, foreign: bool):
+        df_qcew = self.base_data().filter(pl.col("year") >= 2012)
+        if naics == "31-33":
+            df_qcew = df_qcew.filter(
+                (pl.col("naics_code").str.starts_with("31"))
+                | (pl.col("naics_code").str.starts_with("32"))
+                | (pl.col("naics_code").str.starts_with("33"))
+            )
+        elif naics == "44-45":
+            df_qcew = df_qcew.filter(
+                (pl.col("naics_code").str.starts_with("44"))
+                | (pl.col("naics_code").str.starts_with("45"))
+            )
+        elif naics == "48-49":
+            df_qcew = df_qcew.filter(
+                (pl.col("naics_code").str.starts_with("48"))
+                | (pl.col("naics_code").str.starts_with("49"))
+            )
+        elif naics == "72-accommodation":
+            df_qcew = df_qcew.filter(
+                (pl.col("naics_code").str.starts_with("7211"))
+                | (pl.col("naics_code").str.starts_with("7212"))
+                | (pl.col("naics_code").str.starts_with("7213"))
+            )
+        elif naics == "72-food":
+            df_qcew = df_qcew.filter(
+                (pl.col("naics_code").str.starts_with("7223"))
+                | (pl.col("naics_code").str.starts_with("7224"))
+                | (pl.col("naics_code").str.starts_with("7225"))
+            )
+        else:
+            df_qcew = df_qcew.filter(pl.col("naics_code").str.starts_with(naics))
+
+        df_qcew = df_qcew.filter(pl.col("ein") != "")
+        pr_zips = self.spatial_data()["zipcode"].to_list()
+
+        df_qcew = df_qcew.with_columns(
+            foreign=pl.when(pl.col("ui_addr_5_zip").is_in(pr_zips)).then(0).otherwise(1)
+        )
+
+        if foreign:
+            df_qcew = df_qcew.filter(pl.col("foreign") == 1)
+        else:
+            df_qcew = df_qcew.filter(pl.col("foreign") == 0)
+
+        df_zips = pl.DataFrame(self.spatial_data().drop("geometry", axis=1))
+
+        df_qcew = df_qcew.join(df_zips, on=["zipcode"], how="inner", validate="m:1")
+
+        df_qcew = df_qcew.group_by(["year", "qtr", "area_fips"]).agg(
+            total_employment=pl.col("total_employment").sum(),
+            total_wages=pl.col("total_wages").sum(),
+        )
+
+        df_qcew = df_qcew.with_columns(
+            avg_wkly_wage=pl.col("total_wages") / (pl.col("total_employment") * 13),
+            min_wage=pl.when((pl.col("year") >= 2002) & (pl.col("year") < 2010))
+            .then(5.15 * 40)
+            .when((pl.col("year") >= 2010) & (pl.col("year") < 2023))
+            .then(7.25 * 40)
+            .when(pl.col("year") == 2023)
+            .then(8.5 * 40)
+            .when(pl.col("year") == 2024)
+            .then(10.5 * 40)
+            .otherwise(-1),
+        )
+        df_qcew = df_qcew.with_columns(
+            k_index=pl.col("min_wage") / pl.col("avg_wkly_wage")
+        )
+
+        data = df_qcew.to_pandas().copy()
+
+        data = data.sort_values(["year", "qtr", "area_fips"]).reset_index(drop=True)
+
+        return data
